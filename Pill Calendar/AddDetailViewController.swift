bd14e7b7bdfe51f733643159880fb3e5a23a7736
AkisTacos <saito.aki93@gmail.com>
2018-03-31 14:57:48 -0700

    Added 'AddDetailViewController'

---
+++ Pill Calendar/AddDetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AddDetailViewController.swift
+//  Pill Calendar
+//
+//  Created by Aki Saito on 3/31/18.
+//  Copyright © 2018 CHRYAKI. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import SVProgressHUD
+
+class AddDetailViewController : UIViewController {
+
+}
+
